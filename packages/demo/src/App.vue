b237328dc02f8dfbb705ccfaf9c2da4849863fbe
Kagol <kagol@sina.com>
2025-01-04 12:40:50 +0800

    feat: add quill demo

---
+++ packages/demo/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import QuillDemo from './components/QuillDemo.vue'
+</script>
+
+<template>
+  <QuillDemo />
+</template>
---
+++ packages/demo/src/components/QuillDemo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Quill from 'quill'
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const quill = new Quill('#editor', {
+    theme: 'snow'
+  });
+})
+</script>
+
+<template>
+  <div id="editor"></div>
+  <div></div>
+</template>
